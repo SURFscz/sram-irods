34a94400d7becc5ad8bb24bba21aa719685dc696
Harry Kodden <harry@kodden.nl>
2020-12-07 22:56:11 +0100

    wip

--- src/irods/app/run_icommands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -z "$IRODS_ZONE" ]
-then
-    sed -i "s/tempZone/$IRODS_ZONE/g" /home/mara/.irods/irods_environment.json
-    sed -i "s/tempZone/$IRODS_ZONE/g" /home/ayub/.irods/irods_environment.json
-fi
-
-sleep infinity
--- src/irods/app/setup_service_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-( echo irods
-  echo irods ) | /var/lib/irods/packaging/setup_irods_service_account.sh
